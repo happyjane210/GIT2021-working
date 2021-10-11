7073a62e665b7a4623949243d0a00ba7e88fdfe0
Jane <happyjane210@gmail.com>
2021-10-12 05:42:35 +0900

    2021-10-12

--- 8. msa/workspace/helloclient/src/main/java/com/git/helloclient/HelloClientService.java
+++ 8. msa/workspace/helloclient/src/main/java/com/git/helloclient/HelloClientService.java
@@ -1,5 +1,7 @@
 package com.git.helloclient;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,34 +14,36 @@ public class HelloClientService {
 
 	private Map<String, SseEmitter> emitters = new HashMap<String, SseEmitter>();
 
-	public void putSseClient(String clientId, SseEmitter emitter) {
+	public void putEmitter(String clientId, SseEmitter emitter) {
 		this.emitters.put(clientId, emitter);
 		System.out.println("emitters: " + emitters.size());
 	}
 
-	public void removeSseClient(String clientId) {
+	public SseEmitter getEmitter(String clientId) {
+		return this.emitters.get(clientId);
+	}
+
+	public void removeEmitter(String clientId) {
 		this.emitters.remove(clientId);
 		System.out.println(emitters.size());
 	}
 
-	// 송신 내번호
+	// 1. 수신 내번호
 	@RabbitListener(queues = "test.hello.2")
-	public void receiveMessage2(String message) {
+	public void receiveMessage2(String message) throws UnsupportedEncodingException {
+
 		System.out.println("— test.hello.2 —");
 		System.out.println(message);
+
+		emitters.values().parallelStream().forEach(emitter -> {
+			try {
+				emitter.send(message);
+			} catch (IOException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		});
+
 	}
 
-	// 송신 재윤님 번호
-//	@RabbitListener(queues = "test.hello.1")
-//	public void receiveMessage1(String message) {
-//		System.out.println("— test.hello.1 —");
-//		System.out.println(message);
-//	}
-//
-//	// 송신 준호님 번호
-//	@RabbitListener(queues = "test.hello.3")
-//	public void receiveMessage3(String message) {
-//		System.out.println("— test.hello.3 —");
-//		System.out.println(message);
-//	}
 }
